3eef76c4766ee8f5b20642e443467a6faea9cad9
Andrés Muñoz <andres2078@gmail.com>
2026-08-03 13:50:18 -0500

    chore: install and configure Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-
 function App() {
-
-
   return (
     <>
-      <h1>Control de gastos</h1>
+      <h1 className="text-6xl">Control de gastos</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
