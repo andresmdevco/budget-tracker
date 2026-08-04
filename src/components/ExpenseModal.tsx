cbb3b8220ed68ce10ac5b97f03aef8dcf04e513d
Andrés Muñoz <andres2078@gmail.com>
2026-08-03 19:37:12 -0500

    feat: open expense modal from action button

--- src/components/ExpenseModal.tsx
+++ src/components/ExpenseModal.tsx
@@ -1,21 +1,24 @@
-import { Fragment } from 'react'
-import { PlusCircleIcon } from '@heroicons/react/24/solid'
-import { Dialog, DialogPanel, Transition, TransitionChild } from '@headlessui/react'
+import { Fragment } from 'react';
+import { PlusCircleIcon } from '@heroicons/react/24/solid';
+import { Dialog, DialogPanel, Transition, TransitionChild } from '@headlessui/react';
+import { useBudget } from '../hooks/useBudget';
 
 export default function ExpenseModal() {
+  const { state, dispatch } = useBudget();
 
   return (
     <>
       <div className="fixed right-5 bottom-5 flex items-center justify-center">
         <button
           type="button"
-          className='cursor-pointer'
+          className="cursor-pointer"
+          onClick={() => dispatch({ type: 'show-modal' })}
         >
-          <PlusCircleIcon className='w-16 h-16 text-blue-600 rounded-full' />
+          <PlusCircleIcon className="w-16 h-16 text-blue-600 rounded-full" />
         </button>
       </div>
 
-      <Transition appear show={false} as={Fragment}>
+      <Transition appear show={state.modal} as={Fragment}>
         <Dialog as="div" className="relative z-10" onClose={() => {}}>
           <TransitionChild
             as={Fragment}
@@ -40,16 +43,12 @@ export default function ExpenseModal() {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <DialogPanel className="w-full max-w-3xl transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-    
-
-    
-                </DialogPanel>
+                <DialogPanel className="w-full max-w-3xl transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"></DialogPanel>
               </TransitionChild>
             </div>
           </div>
         </Dialog>
       </Transition>
     </>
-  )
+  );
 }
--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -1,23 +1,31 @@
-
-export type BudgetActions = 
-  { type: 'add-budget', payload: {budget: number} }
+export type BudgetActions =
+  | { type: 'add-budget'; payload: { budget: number } }
+  | { type: 'show-modal' };
 
 export type BudgetState = {
-  budget: number
-}
+  budget: number;
+  modal: boolean;
+};
 
 export const initialState: BudgetState = {
-  budget: 0
-}
+  budget: 0,
+  modal: false,
+};
 
-export const budgetReducer = (state: BudgetState = initialState, action: BudgetActions) => {  
-
+export const budgetReducer = (state: BudgetState = initialState, action: BudgetActions) => {
   if (action.type === 'add-budget') {
     return {
       ...state,
-      budget: action.payload.budget
-    }
+      budget: action.payload.budget,
+    };
   }
 
-  return state
-}
+  if (action.type === 'show-modal') {
+    return {
+      ...state,
+      modal: true,
+    };
+  }
+
+  return state;
+};
