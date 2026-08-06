a4ed36be91691d56e65da221bedaf5a1040fe114
Andrés Muñoz <andres2078@gmail.com>
2026-08-05 23:56:47 -0500

    style: center empty expense list message

--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -12,7 +12,7 @@ export default function ExpenseList() {
   return (
     <div className="mt-10 bg-white shadow-lg rounded-lg p-10">
       {isEmpty ? (
-        <p className="text-gray-600 text-2xl font-bold">No Hay Gastos</p>
+        <p className="text-gray-600 text-2xl font-bold text-center">No Hay Gastos</p>
       ) : (
         <>
           <p className="text-gray-600 text-2xl font-bold my-5">Listado de Gastos</p>
