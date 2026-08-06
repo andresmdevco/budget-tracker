b42ee20bdd35fddd2615464bf3076ca4fc70ffd2
Andrés Muñoz <andres2078@gmail.com>
2026-08-05 23:18:34 -0500

    feat: filter expenses by selected category

--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -4,8 +4,10 @@ import ExpenseDetail from './ExpenseDetail';
 
 export default function ExpenseList() {
   const { state } = useBudget();
-
-  const isEmpty = useMemo(() => state.expenses.length === 0, [state.expenses]);
+  const filteredExpenses = state.currentCategory
+    ? state.expenses.filter((expense) => expense.category === state.currentCategory)
+    : state.expenses;
+  const isEmpty = useMemo(() => filteredExpenses.length === 0, [filteredExpenses]);
 
   return (
     <div className="mt-10 bg-white shadow-lg rounded-lg p-10">
@@ -13,8 +15,8 @@ export default function ExpenseList() {
         <p className="text-gray-600 text-2xl font-bold">No Hay Gastos</p>
       ) : (
         <>
-          <p className="text-gray-600 text-2xl font-bold my-5">Listado de Gastos.</p>
-          {state.expenses.map((expense) => (
+          <p className="text-gray-600 text-2xl font-bold my-5">Listado de Gastos</p>
+          {filteredExpenses.map((expense) => (
             <ExpenseDetail key={expense.id} expense={expense} />
           ))}
         </>
