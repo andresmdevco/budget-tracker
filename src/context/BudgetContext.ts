573d0dc026e7a455b0bd54ed993c40355e49cea8
Andrés Muñoz <andres2078@gmail.com>
2026-08-05 23:48:19 -0500

    refactor: separate BudgetContext from BudgetProvider

---
+++ src/context/BudgetContext.ts
@@ -0,0 +1,12 @@
+import { createContext, type Dispatch } from 'react';
+import type { BudgetActions, BudgetState } from '../reducers/budget-reducer';
+
+type BudgetContextProps = {
+  state: BudgetState;
+  dispatch: Dispatch<BudgetActions>;
+  totalExpenses: number;
+  remainingBudget: number;
+};
+
+export const BudgetContext = createContext<BudgetContextProps>(null!);
+
--- src/context/BudgetProvider.tsx
+++ src/context/BudgetProvider.tsx
@@ -1,23 +1,11 @@
-import { useReducer, createContext, type Dispatch, type ReactNode, useMemo } from 'react';
-import {
-  budgetReducer,
-  initialState,
-  type BudgetActions,
-  type BudgetState,
-} from '../reducers/budget-reducer';
+import { useMemo, useReducer, type ReactNode } from "react";
+import { budgetReducer, initialState } from "../reducers/budget-reducer";
+import { BudgetContext } from "./BudgetContext";
 
-type BudgetContextProps = {
-  state: BudgetState;
-  dispatch: Dispatch<BudgetActions>;
-  totalExpenses: number;
-  remainingBudget: number;
-};
 
 type BudgetProviderProps = {
   children: ReactNode;
 };
-
-export const BudgetContext = createContext<BudgetContextProps>(null!);
 
 export const BudgetProvider = ({ children }: BudgetProviderProps) => {
   const [state, dispatch] = useReducer(budgetReducer, initialState);
